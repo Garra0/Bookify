b5be5ecddf81a9543d5e660c769968ce1e1ddb6c
Garra0 <osama2002amjad@gmail.com>
2026-07-03 09:26:33 +0300

    Initial commit

---
+++ Bookify.Domain/Apartments/Amenity.cs
@@ -0,0 +1,15 @@
+﻿namespace Bookify.Domain.Apartments;
+
+public enum Amenity
+{
+    Wifi = 1,
+    AirCoditioning = 2,
+    Parking = 3,
+    PetFriedly = 4,
+    SwimmingPool = 5,
+    Gym = 6,
+    Spa = 7,
+    Terrace = 8,
+    MountainView = 9,
+    GardenView = 10,
+}
---
+++ Bookify.Domain/Apartments/Currency.cs
@@ -0,0 +1,25 @@
+﻿namespace Bookify.Domain.Apartments;
+
+
+public record Currency
+{
+    private Currency(string code) => Code = code;
+
+    public string Code { get; set; }
+
+    public static readonly Currency Usd = new("USD");
+    public static readonly Currency Eur = new("EUR");
+    internal static readonly Currency None = new("");
+
+    public static Currency FromCode(string code)
+    {
+        return All.FirstOrDefault(x => x.Code == code) ??
+            throw new ApplicationException("The currency code is invalid");
+    }
+
+    public static readonly IReadOnlyCollection<Currency> All =
+    [
+        Usd,
+        Eur
+    ];
+}
